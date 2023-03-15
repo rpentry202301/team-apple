8df34abcd79c489d368dba551e3795cdb5b66cce
suzu784 <s.suzuki784@hotmail.com>
2023-03-15 14:18:07 +0900

    ログイン、ログアウト後のフラッシュメッセージを出す

--- src/php/resources/views/home.blade.php
+++ src/php/resources/views/home.blade.php
@@ -5,7 +5,6 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +13,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('あなたは既にログインしています。') }}
                 </div>
             </div>
         </div>
